f2d9cfd444e95d67d19d610a7c5c1567fc0cd704
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-05-14 17:44:38 +0530

    Layouts in Next.js

---
+++ 02/my-app/app/(admin)/adminDashboard/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminDash = () => {
+  return (
+    <div>This is Admin Dashboard</div>
+  )
+}
+
+export default AdminDash
---
+++ 02/my-app/app/(admin)/adminLogin/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminLogin = () => {
+  return (
+    <div>This is Admin Login Page...</div>
+  )
+}
+
+export default AdminLogin
